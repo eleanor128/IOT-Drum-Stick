312f6e2dd25a31667898e031c70c15d4ad5c4ea6
eleanor_lab <eleanor128@gmail.com>
2025-12-13 13:08:30 +0800

    adj drum position

--- static/js/drum_3d.js
+++ static/js/drum_3d.js
@@ -73,7 +73,7 @@ let drumMeshes = {};
 let rightStick, leftStick;
 
 const zones = [
-    { name: "Hihat",     x: 675, y: 225, w: 225, h: 225, color:"#3232ff", pos3d: [3, 1, -0.8], radius: 1.0, rotation: -Math.PI / 9 },
+    { name: "Hihat",     x: 675, y: 225, w: 225, h: 225, color:"#3232ff", pos3d: [2.5, 1, -0.8], radius: 1.0, rotation: -Math.PI / 9 },
     { name: "Snare",     x: 450, y: 225, w: 225, h: 225, color:"#d9d9d9", pos3d: [1, 0.2, -0.8], radius: 1, rotation: -Math.PI / 12 },
     { name: "Tom_high",  x: 450, y: 0,   w: 225, h: 225, color:"#ff7f2a", pos3d: [1, 1, 1.5], radius: 1, rotation: -Math.PI / 7 },
     { name: "Tom_mid",   x: 450, y: 0,   w: 225, h: 225, color:"#ff7f2a", pos3d: [-1, 1, 1.5], radius: 1, rotation: -Math.PI / 7 },
